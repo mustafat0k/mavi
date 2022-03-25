f00fbc6a3dcb6c9f95ff78cbb8541fcf6e0eef30
Hyper <mustafatooook@gmail.com>
2022-03-26 00:14:45 +0300

    dashboard

--- server/src/owners/dto/create-owner.input.ts
+++
@@ -1,8 +0,0 @@
-import { InputType, Int, Field } from '@nestjs/graphql';
-import { Column } from 'typeorm';
-
-@InputType()
-export class CreateOwnerInput {
-  @Field()
-  name: string;
-}
--- server/src/words/dto/update-word.input.ts
+++
@@ -1,8 +0,0 @@
-import { CreateWordInput } from './create-word.input';
-import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
-
-@InputType()
-export class UpdateWordInput extends PartialType(CreateWordInput) {
-  @Field(() => Int)
-  id: number;
-}
